4fa1115ce315a695c0b60a07c1c15fad8c9ab65b
Edwin <usss017222@ugb.edu.sv>
2022-07-26 17:47:50 -0600

    Modificaciones finales

--- TareaSemana2.Designer.cs
+++ TareaSemana2.Designer.cs
@@ -30,32 +30,46 @@ namespace Primer_Proyecto_Visualcshart_Formularios
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(269, 107);
+            this.button1.Location = new System.Drawing.Point(176, 74);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(130, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Haz Clic Aqui";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // Form1
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(136, 134);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(219, 154);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "INTEGRANTES:\r\n1. Edwin Lopez\r\n2. Christian Montenegro\r\n3. Nilderson Chavez\r\n4. Di" +
+    "ego Zepeda\r\n5. Henry Sanchez\r\n6. Fernando Lopez\r\n";
+            // 
+            // TareaSemana2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(550, 396);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
-            this.Name = "Form1";
+            this.Name = "TareaSemana2";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
